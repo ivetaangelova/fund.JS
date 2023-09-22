9dcfc64dd2dbc869fb3ec84a95bd34192c923aca
Iveta Angelova <145490215+ivetaangelova@users.noreply.github.com>
2023-09-22 19:25:43 +0300

    add new solutions & change name

---
+++ Data Types and Variables/Lab/triplesOfLatinLetters.js
@@ -0,0 +1,15 @@
+function solve(nAsString) {
+    let num = Number(nAsString);
+    let chars = `abcdefghijklmnopqrstuvwxyz`
+    for (let n1 = 0; n1 < num; n1++) {
+
+        for (let n2 = 0; n2 < num; n2++) {
+
+            for (let n3 = 0; n3 < num; n3++) {
+                console.log(chars[n1] + chars[n2] + chars[n3]);
+            }
+        }
+    }
+    // let latter=String.fromCharCode(97+ ...arguments.)
+}
+solve('3')
