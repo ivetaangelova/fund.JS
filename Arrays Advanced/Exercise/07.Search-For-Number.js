2a10bf1da23a501171808dc7f022254ccd25c6fb
Iveta Angelova <145490215+ivetaangelova@users.noreply.github.com>
2023-10-31 12:31:09 +0200

    add exercise

--- Arrays Advanced/Exercise/07.Search-For-Number.js
+++ Arrays Advanced/Exercise/07.Search-For-Number.js
@@ -1,5 +1,21 @@
-function searchNum() {
-
+// function searchNum(arr, elements) {
+// const [toTake, deleteCount,searchNum] = elements;
+// const newArr= arr.slice(0, toTake);
+// newArr.splice(0, deleteCount);
+// let occursCount=0;
+// for(num of newArr){
+//     if(num==searchNum){
+//         occursCount++;
+//     }
+// }
+// console.log(`Number ${searchNum} occurs ${occursCount} times.`)
+// }
+function searchNum(arr, elements) {
+    const [newArrLenght, remove, number] = elements;
+    const newArr = arr.slice(0, newArrLenght);
+    newArr.splice(0, remove);
+    const occursCount = newArr.filter(x => x == number).length;
+    console.log(`Number ${number} occurs ${occursCount} times.`);
 }
 searchNum(
     [7, 1, 5, 8, 2, 7],
